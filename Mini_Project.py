453901c4d48ad07d5acc053a5307677e6275fcba
Rene <renegiragosyan@gmail.com>
2025-03-27 21:25:35 +0400

    Rolling dice game

--- Mini_Project.py
+++ Mini_Project.py
@@ -18,37 +18,47 @@ def roll_the_dice(x, y) -> int:
 
 def player():
     input('Press Enter to roll the dices')
+    
     return int(roll_the_dice(diceOne(), diceTwo()))
 
 
 def playing_rules():
     one_shot = 0
-    goal = 0
-
-    while goal < 2 or one_shot < 1:
+    point = 0
+    goal_num = 0
+    while point < 2 and one_shot < 1:
 
         x = player()
+        goal_num += x
+        if (x == 4 or x == 5 or x == 6 or x == 8 or x == 9 or x == 10) and point == 0:
+            point += 1
+            print('Point', point, 'Dice gives', x, 'Your goal number is', goal_num)
+            print('-------------')            
 
-        if goal == 0 or goal == 1:
-            if x == 4 or x == 5 or x == 6 or x == 8 or x == 9 or x == 10:
-                goal = goal + 1
-                print('Sum of Dices:', x, "| goal is", goal)
-                if goal == 2:
-                    print("GOAAAL Player wins ")
-                    break
-            elif x == 2 or x == 3 or x == 12:
-                one_shot = one_shot + 1
-                print('Sum of Dices:', x, '| The Casino Wins')
-                break
-            elif goal == 0 and (x == 7 or x == 11):
-                one_shot = one_shot + 1
-                print('Sum of Dices:', x, ' | Player has no goal, Lost')
-                break
-            elif goal == 1 and (x == 7 or x == 11):
-                goal = goal + 1
-                print('Sum of Dices:', x, '| goal is ', goal)
-                print('Player Wins')
-                break
+        elif point == 1 and (x == 4 or x == 5 or x == 6 or x == 8 or x == 9 or x == 10):
+            goal_num = goal_num - x
+            print('Dice gives',x,'your goal number is', goal_num)
+            if goal_num == x:
+                point += 1
+                print("Point", point, 'your goal number is', goal_num, 'PLAYER WINS')
+                print('-------------')            
+                
+        
+        elif x == 2 or x == 3 or x == 12:
+            print('Point' ,point, 'Craps, CASINO WINS', "Dice gave", x)
+            one_shot += 1
+            
+        
+        elif point == 0 and (x == 7 or x == 11):
+            one_shot += 1
+            print('Point', point, 'Player lost :( Dice gave', x, 'yout goal number was', goal_num)
+            
+        
+        elif point > 0 and (x == 7 or x == 11):
+            point += 1
+            print("Point", point, 'PLAYER WINS', x, 'your goal number was', goal_num)
+            print('-------------')            
+
     return 'End Game'
 
 
